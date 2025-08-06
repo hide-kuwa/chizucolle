746c2dfde2484719555e519ead48f249d228da2d
hide-kuwa <hideyuki.kuwayama@gmai.com>
2025-08-06 15:09:51 +0900

    first commit

---
+++ src/stories/Button.tsx
@@ -0,0 +1,41 @@
+import './button.css';
+
+export interface ButtonProps {
+  /** Is this the principal call to action on the page? */
+  primary?: boolean;
+  /** What background color to use */
+  backgroundColor?: string;
+  /** How large should the button be? */
+  size?: 'small' | 'medium' | 'large';
+  /** Button contents */
+  label: string;
+  /** Optional click handler */
+  onClick?: () => void;
+}
+
+/** Primary UI component for user interaction */
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+      <button
+    type="button"
+    // Tailwind CSSの魔法を追加！ hover:opacity-80 を足してみよう
+    className={[
+      'storybook-button',
+      `storybook-button--${size}`,
+      mode,
+      'transition-opacity hover:opacity-80', // ← この行を追加！
+    ].join(' ')}
+    style={{ backgroundColor }}
+    {...props}
+  >
+    {label}
+  </button>
+  );
+};
